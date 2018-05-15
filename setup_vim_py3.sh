1fd2a43a0c41e4fda758235170b336bdd8024850
Jayan Brahmanandan <jayan.brahmanandan@reancloud.com>
2018-05-15 19:29:57 +0530

    setup: download vimrc from repo

--- setup_vim_py3.sh
+++ setup_vim_py3.sh
@@ -5,9 +5,12 @@ echo "Configuring EPEL Repository"
 yum install epel-release -y
 
 # Install vim, python3 and necessary tools
-yum install vim python-36 virtualenvwrapper git wget curl tree
+yum install vim python-36 virtualenvwrapper git wget curl tree tmux
 
 # Download color scheme
 mkdir -p ~/.vim/colors 
 wget -O ~/.vim/colors/wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
+
+# Download vimrc
+wget -O ~/.vimrc https://raw.githubusercontent.com/jayanatl/vim_python/master/vimrc
 
