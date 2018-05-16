1ed9b102a48fa62f10562f5c18c5d134063bf214
Jayan Brahmanandan <jayan.brahmanandan@reancloud.com>
2018-05-17 00:15:26 +0530

    read test

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # Auto setup development environment
 
 ```
-curl -L https://raw.githubusercontent.com/jayanatl/vim_python/initial_prototype/install_centos.sh|bash
+bash <(curl -L https://raw.githubusercontent.com/jayanatl/vim_python/initial_prototype/install_centos.sh)
 ```
--- setup_centos.sh
+++ setup_centos.sh
@@ -2,12 +2,6 @@
 
 source ./lib_sh/echos.sh
 bot "Hi! I'm going to setup this machine. Here I go..."
-read -r -p "What is your github.com username? " githubuser
-read -r -p "What is your first name? " firstname
-read -r -p "What is your last name? " lastname
-read -r -p "What is your email? " email
-fullname="$firstname $lastname"
-bot "Greatings $fullname, "
 
 bot "Configuring EPEL Repository"
 sudo yum install epel-release -y
@@ -32,6 +26,12 @@ clear
 bot "Configuring git"
 grep 'user = GITHUBUSER' ./homedir/.gitconfig > /dev/null 2>&1
 if [[ $? = 0 ]]; then
+    read -r -p "What is your github.com username? " githubuser
+    read -r -p "What is your first name? " firstname
+    read -r -p "What is your last name? " lastname
+    read -r -p "What is your email? " email
+    fullname="$firstname $lastname"
+    bot "Greatings $fullname, "
     sed -i "s/GITHUBFULLNAME/$firstname $lastname/" ./homedir/.gitconfig > /dev/null 2>&1
     sed -i 's/GITHUBEMAIL/'$email'/' ./homedir/.gitconfig
     sed -i 's/GITHUBUSER/'$githubuser'/' ./homedir/.gitconfig
