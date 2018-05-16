60a7ce11fee3198f42dc64cee149beb11d561adc
Jayan Brahmanandan <jayan.brahmanandan@reancloud.com>
2018-05-16 23:46:19 +0530

    fix temp folder

--- install_centos.sh
+++ install_centos.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+rm -rf /tmp/repo &>/dev/null
 mkdir -p /tmp/repo
-pushd /tmp/repo
+cd /tmp/repo
 curl -L https://github.com/jayanatl/vim_python/archive/initial_prototype.zip > repo.zip
 yum -y install unzip
 unzip repo.zip
 rm repo.zip
 mv vim_python* ~/.dotfiles
 cd ~/.dotfiles
+rm -rf /tmp/repo
 bash setup_centos.sh
-popd
