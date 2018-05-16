13788683af6a59c801f26001993bca8c11a98acc
Jayan Brahmanandan <jayan.brahmanandan@reancloud.com>
2018-05-16 23:04:41 +0530

    add install_centos.sh

---
+++ install_centos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p /tmp/repo
+pushd /tmp/repo
+curl -L https://github.com/jayanatl/vim_python/archive/initial_prototype.zip > repo.zip
+yum -y install unzip
+unzip repo.zip
+rm repo.zip
+mv vim_python* ~/.dotfiles
+cd ~/.dotfiles
+sudo bash setup_centos.sh
+popd
