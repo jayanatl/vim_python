204608f32b7351ae05b8c32e99e497ee7d8cf23c
Jayan <jayanatl@gmail.com>
2020-04-26 01:47:41 -0400

    fix

--- 10-backup-dotfiles/install.sh
+++ 10-backup-dotfiles/install.sh
@@ -18,10 +18,10 @@ is_sudo
 
 OS=$(this_os)
 
+if [[ ${OP} == "install" ]]; then
 ############################
 # Install                  #
 ############################
-[[ ${OP} == "install" ]] && {
   echo "Baking up dot files that are being replaced"
   # Cleanup brew entry in dot.profile/dot.bashrc
   # Get that entry from current .bashrc/.profile before moving it to backup
@@ -29,13 +29,14 @@ OS=$(this_os)
   # Create org if not present
   # Create backup if files which are not simlinks to .dotfiles and is getting replaced
   #   and move those to backup folder
-}
 
+
+elif [[ ${OP} == "uninstall" ]]; then
 ############################
 # Uninstall                #
 ############################
-[[ ${OP} == "uninstall" ]] && {
   echo "Restoring old dotfiles from backup"
   # Resetore files in reverse order
-}
+
+fi
 
